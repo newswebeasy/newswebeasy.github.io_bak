c70df1628419153d876fdd328ad947f0444ba4cd
NewsWebEasy <newswebeasy@users.noreply.github.com>
2017-11-05 21:49:25 +0800

    http://www3.nhk.or.jp/news/html/20171105/k10011211571000.html

---
+++ assets/slider.js
@@ -0,0 +1,20 @@
+var slider = document.getElementById('slider');
+if (slider) {
+  slider.oninput = function(event) {
+    var value = parseInt(event.target.value);
+
+    var label = document.getElementById('level');
+    label.innerHTML = value;
+
+    for (var i = 1; i < 10; i++) {
+      var l = document.querySelectorAll('rt[data-level="' + i + '"]');
+      for (var j = 0; j < l.length; j++) {
+        if (i < value) {
+          l[j].classList.add('hidden');
+        } else {
+          l[j].classList.remove('hidden');
+        }
+      }
+    }
+  }
+}
