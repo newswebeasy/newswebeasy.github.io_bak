4c25de4eae02ee624e399383dcddce753cc2e653
NewsWebEasy <newswebeasy@users.noreply.github.com>
2017-11-06 07:16:08 +0800

    default slider value

--- assets/slider.js
+++ assets/slider.js
@@ -1,20 +1,26 @@
-var slider = document.getElementById('slider');
-if (slider) {
-  slider.oninput = function(event) {
-    var value = parseInt(event.target.value);
+function setLevel(level) {
+  var label = document.getElementById('level');
+  label.innerHTML = value;
 
-    var label = document.getElementById('level');
-    label.innerHTML = value;
-
-    for (var i = 1; i < 10; i++) {
-      var l = document.querySelectorAll('rt[data-level="' + i + '"]');
-      for (var j = 0; j < l.length; j++) {
-        if (i < value) {
-          l[j].classList.add('hidden');
-        } else {
-          l[j].classList.remove('hidden');
-        }
+  for (var i = 1; i < 10; i++) {
+    var l = document.querySelectorAll('rt[data-level="' + i + '"]');
+    for (var j = 0; j < l.length; j++) {
+      if (i < value) {
+        l[j].classList.add('hidden');
+      } else {
+        l[j].classList.remove('hidden');
       }
     }
   }
 }
+
+var slider = document.getElementById('slider');
+if (slider) {
+  slider.oninput = function(event) {
+    var value = parseInt(event.target.value);
+    setLevel(value);
+  }
+
+  slider.value = '4';
+}
+
